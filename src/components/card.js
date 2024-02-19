3ec5ce1601e19b7f3b530056649403bf549d49ca
llcain <llcain2012@gmail.com>
2024-02-18 19:31:41 -0600

    Styled form container

--- src/components/card.js
+++ src/components/card.js
@@ -1,9 +1,13 @@
 import React from "react";
+import cloudy from "../images/cloudy.jpg";
 
 
 const Card = (props) => {
     return (
         <div className="card-container" style={{border: '2px solid black', margin: '10px', width: '350px'}}>
+            <div className="image-container">
+                <img className="card-image" src={cloudy} alt="cloudy"/>
+            </div>
             <div className="card-description">
                 <p>{props.name}</p>
                 <p>{props.age}</p>
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,12 +1,12 @@
 const Contact = () => {
     return (
-        <div>
+        <div  className="form-container">
             <h1>Contact Us page</h1>
             <form action="post">
                 <label for="fname">First Name:</label><br/>
                 <input type="text" id="fname" name="fname"/><br/>
                 <label for="lname">Last Name:</label><br/>
-                <input type="text" id="lname" name="lname"/><br/>
+                <input type="text" id="lname" name="lname"/><br/><br/>
                 <input type="submit" value="Submit"/>
             </form>
         </div>
